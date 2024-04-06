6bd4d9995b0a5c1ef1e2faa78d1e95d4f81f070b
程同学 <zenos.chen@foxmail.com>
2024-04-06 19:23:17 +0800

    fix bug

--- src/main.js
+++ src/main.js
@@ -1,110 +1,100 @@
-import * as THREE from 'three'
-import Stat from 'three/examples/jsm/libs/stats.module'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import * as dat from 'dat.gui'
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
-import { FontLoader } from 'three/addons/loaders/FontLoader.js';
-import jsonUrl2 from './static/fonts/helvetiker_regular.typeface.json?url'
-const w = window.innerWidth
-const h = window.innerHeight
-const stat = new Stat()
-const gui = new dat.GUI()
+import * as THREE from "three";
+import Stat from "three/examples/jsm/libs/stats.module";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as dat from "dat.gui";
+import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
+import { FontLoader } from "three/addons/loaders/FontLoader.js";
+import fontJsonUrl from "./static/fonts/helvetiker_regular.typeface.json?url";
+import _8png from "./static/textures/matcaps/8.png?url";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const stat = new Stat();
+const gui = new dat.GUI();
 
 //Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-//Camera 
-const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 10)
-camera.lookAt(0, 0, 0)
-camera.position.set(2, 2, 3)
+//Camera
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 10);
+camera.lookAt(0, 0, 0);
+camera.position.set(2, 2, 3);
 
 //辅助坐标系
 //scene.add(new THREE.AxesHelper(2, 2, 2))
 
 //Text
-const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('./static/textures/matcaps/8.png')
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load(_8png);
 
 //Font
-const fontLoader = new FontLoader()
-const jsonURL = new URL('./static/fonts/helvetiker_regular.typeface.json',import.meta.url);
-console.log(jsonURL,jsonUrl2)
+const fontLoader = new FontLoader();
+
 fontLoader.load(
-    '/helvetiker_regular.typeface.json',
-    // jsonUrl2,
-    (font) =>
-    {
-        // Material
-        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+	fontJsonUrl,
+	(font) => {
+		// Material
+		const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
 
-        // Text
-        const textGeometry = new TextGeometry(
-            'I am a student ',
-            {
-                font: font,
-                size: 0.5,
-                height: 0.2,
-                curveSegments: 12,
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 5
-            }
-        )
-        textGeometry.center()
+		// Text
+		const textGeometry = new TextGeometry("I am a student ", {
+			font: font,
+			size: 0.5,
+			height: 0.2,
+			curveSegments: 12,
+			bevelEnabled: true,
+			bevelThickness: 0.03,
+			bevelSize: 0.02,
+			bevelOffset: 0,
+			bevelSegments: 5,
+		});
+		textGeometry.center();
 
-        const text = new THREE.Mesh(textGeometry, material)
-        scene.add(text)
+		const text = new THREE.Mesh(textGeometry, material);
+		scene.add(text);
 
-         //Donuts
-       const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
+		//Donuts
+		const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64);
 
-         for(let i = 0; i < 99; i++)
-        {
-            const donut = new THREE.Mesh(donutGeometry, material)
-            donut.position.x = (Math.random() - 0.5) * 10
-            donut.position.y = (Math.random() - 0.5) * 10
-            donut.position.z = (Math.random() - 0.5) * 10
-            donut.rotation.x = Math.random() * Math.PI
-            donut.rotation.y = Math.random() * Math.PI
-            const scale = Math.random()
-            donut.scale.set(scale, scale, scale)
+		for (let i = 0; i < 99; i++) {
+			const donut = new THREE.Mesh(donutGeometry, material);
+			donut.position.x = (Math.random() - 0.5) * 10;
+			donut.position.y = (Math.random() - 0.5) * 10;
+			donut.position.z = (Math.random() - 0.5) * 10;
+			donut.rotation.x = Math.random() * Math.PI;
+			donut.rotation.y = Math.random() * Math.PI;
+			const scale = Math.random();
+			donut.scale.set(scale, scale, scale);
 
-            scene.add(donut)
-        }
-    }
-)
+			scene.add(donut);
+		}
+	},
+	// onProgress callback
+	function (xhr) {
+		console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+	},
 
-
-
-
-
+	// onError callback
+	function (err) {
+		console.log("An error happened", err);
+	}
+);
 
 //Renderer
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(w, h)
-document.body.append(renderer.domElement)
-document.body.append(stat.dom)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(w, h);
+document.body.append(renderer.domElement);
+document.body.append(stat.dom);
 
-const orbitControls = new OrbitControls(camera, renderer.domElement)
+const orbitControls = new OrbitControls(camera, renderer.domElement);
 
-
-const clock = new THREE.Clock()
-tick()
+const clock = new THREE.Clock();
+tick();
 function tick() {
-    const time = clock.getElapsedTime()
+	const time = clock.getElapsedTime();
 
-
-    requestAnimationFrame(tick)
-    renderer.render(scene, camera)
-    stat.update()
-    orbitControls.update()
+	requestAnimationFrame(tick);
+	renderer.render(scene, camera);
+	stat.update();
+	orbitControls.update();
 }
-
-
-
-
-
-
-
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    server:{
-        port: 8080,
-        open:true
-    }
-})
-
-
-
- 
+	base: "./",
+	server: {
+		port: 8080,
+		open: true,
+	},
+});
